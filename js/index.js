0c8ce93f9288eeedebb807422ddcf7d39d3c766c
Victor Arowo <arowov@gmail.com>
2019-09-03 11:59:13 +0100

    Added First Event Listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+const container = document.querySelector('*');
+const header = document.querySelector('header');
+const nav = document.querySelectorAll('nav a');
+
+
+//Double click header to toggle darkmode
+
+header.addEventListener('dblclick', e  =>  {
+    container.classList.toggle('dark');
+    header.classList.toggle('dark');
+    nav.forEach(a => a.classList.toggle('dark'));
+});
+
+
